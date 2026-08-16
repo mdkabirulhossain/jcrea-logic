08969c73af781bac5bf70d65db9f024ee73614ce
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-08-17 02:37:49 +0600

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -11,12 +11,12 @@ export default auth((req) => {
 
   // 1. Unauthenticated users trying to access protected dashboard routes -> redirect to /login
   if (isOnDashboard && !isLoggedIn) {
-    return NextResponse.redirect(new URL("/login", req.nextUrl));
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
   // 2. Authenticated users trying to access login or root page -> redirect directly to /dashboard
   if ((isOnLogin || isOnRoot) && isLoggedIn) {
-    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   return NextResponse.next();
